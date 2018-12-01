4b6673fd0442572fccf082b05b24756a06d583bb
Shawn Hartley <arret@novusterra.com>
2018-12-01 17:55:03 -0600

    added a shooter

--- Assets/Players/Enemy.cs
+++ Assets/Players/Enemy.cs
@@ -3,56 +3,42 @@ using System.Collections;
 
 namespace Completed
 {
-    //Enemy inherits from MovingObject, our base class for objects that can move, Player also inherits from this.
     public class Enemy : MovingObject
     {
-        public int playerDamage;                            //The amount of food points to subtract from the player when attacking.
-        public AudioClip attackSound1;                      //First of two audio clips to play when attacking the player.
-        public AudioClip attackSound2;                      //Second of two audio clips to play when attacking the player.
-        public AudioClip chopSound1;                //1 of 2 audio clips that play when the wall is attacked by the player.
-        public AudioClip chopSound2;                //2 of 2 audio clips that play when the wall is attacked by the player.
+        public int playerDamage;
+        public AudioClip attackSound1;
+        public AudioClip attackSound2;
+        public AudioClip chopSound1;
+        public AudioClip chopSound2;
 
 
-        private Animator animator;                          //Variable of type Animator to store a reference to the enemy's Animator component.
-        private Transform target;                           //Transform to attempt to move toward each turn.
-        private bool skipMove;                              //Boolean to determine whether or not enemy should skip a turn or move this turn.
+        private Animator animator;
+        private Transform target;
+        private bool skipMove;
         public int hp = 3;
 
 
-        //Start overrides the virtual Start function of the base class.
         protected override void Start()
         {
-            //Register this enemy with our instance of GameManager by adding it to a list of Enemy objects. 
-            //This allows the GameManager to issue movement commands.
             GameManager.instance.AddEnemyToList(this);
-
-            //Get and store a reference to the attached Animator component.
             animator = GetComponent<Animator>();
 
             //Find the Player GameObject using it's tag and store a reference to its transform component.
             target = GameObject.FindGameObjectWithTag("Player").transform;
 
-            //Call the start function of our base class MovingObject.
             base.Start();
         }
 
 
-        //Override the AttemptMove function of MovingObject to include functionality needed for Enemy to skip turns.
-        //See comments in MovingObject for more on how base AttemptMove function works.
         protected override void AttemptMove<T>(int xDir, int yDir)
         {
-            //Check if skipMove is true, if so set it to false and skip this turn.
             if (skipMove)
             {
                 skipMove = false;
                 return;
 
             }
-
-            //Call the AttemptMove function from MovingObject.
             base.AttemptMove<T>(xDir, yDir);
-
-            //Now that Enemy has moved, set skipMove to true to skip next move.
             skipMove = true;
         }
 
@@ -60,8 +46,6 @@ namespace Completed
         //MoveEnemy is called by the GameManger each turn to tell each Enemy to try to move towards the player.
         public void MoveEnemy()
         {
-            //Declare variables for X and Y axis move directions, these range from -1 to 1.
-            //These values allow us to choose between the cardinal directions: up, down, left and right.
             int xDir = 0;
             int yDir = 0;
 
@@ -82,36 +66,24 @@ namespace Completed
 
         public void DamageEnemy(int loss)
         {
-            //Call the RandomizeSfx function of SoundManager to play one of two chop sounds.
             SoundManager.instance.RandomizeSfx(chopSound1, chopSound2);
 
             //Set spriteRenderer to the damaged wall sprite.
             //spriteRenderer.sprite = dmgSprite;
 
-            //Subtract loss from hit point total.
             hp -= loss;
 
-            //If hit points are less than or equal to zero:
             if (hp <= 0)
-                //Disable the gameObject.
+            {
                 gameObject.SetActive(false);
+            }
         }
 
-
-        //OnCantMove is called if Enemy attempts to move into a space occupied by a Player, it overrides the OnCantMove function of MovingObject 
-        //and takes a generic parameter T which we use to pass in the component we expect to encounter, in this case Player
         protected override void OnCantMove<T>(T component)
         {
-            //Declare hitPlayer and set it to equal the encountered component.
             Player hitPlayer = component as Player;
-
-            //Call the LoseFood function of hitPlayer passing it playerDamage, the amount of foodpoints to be subtracted.
             hitPlayer.LoseFood(playerDamage);
-
-            //Set the attack trigger of animator to trigger Enemy attack animation.
             animator.SetTrigger("enemyAttack");
-
-            //Call the RandomizeSfx function of SoundManager passing in the two audio clips to choose randomly between.
             SoundManager.instance.RandomizeSfx(attackSound1, attackSound2);
         }
     }
---
+++ Assets/Weapons/Bullet.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Completed
+{
+    //public class Bullet : MovingObject
+    //{
+    //    static private int baseDamage = 1;
+    //    public int wallDamageModifier = 0;
+    //    public int enemyDamageModifier = 0;
+    //    public int wallDamage;                  //How much damage a player does to a wall when chopping it.
+    //    public int enemyDamage;
+    //    public bool isEnemyShot = false;
+
+    //protected override void Start()
+    //{
+
+    //    GameManager.instance.AddBulletToList(this);
+
+    //    wallDamage = baseDamage + wallDamageModifier;
+    //    enemyDamage = baseDamage + enemyDamageModifier;
+    //    base.Start();
+    //}
+
+    //protected override void OnCantMove<T>(T component)
+    //{
+    //    //throw new System.NotImplementedException();
+    //    if (component is Wall)
+    //    {
+    //        Wall hitWall = component as Wall;
+    //        hitWall.DamageWall(wallDamage);
+    //    }
+    //    else if (component is Enemy)
+    //    {
+    //        Enemy hitEnemy = component as Enemy;
+    //        hitEnemy.DamageEnemy(enemyDamage);
+    //    }
+    //}
+
+    //    private void Update()
+    //    {
+
+    //    }
+    //}
+
+    public class Bullet : MovingObject
+    {
+        public float velX;
+        public float velY = 0f;
+        Rigidbody2D rb2d;
+
+        static private int baseDamage = 1;
+        public int wallDamageModifier = 0;
+        public int enemyDamageModifier = 0;
+        public int wallDamage;                  //How much damage a player does to a wall when chopping it.
+        public int enemyDamage;
+        public bool isEnemyShot = false;
+
+        //private void Start()
+        protected override void Start()
+        {
+            rb2d = GetComponent<Rigidbody2D>();
+            wallDamage = baseDamage + wallDamageModifier;
+            enemyDamage = baseDamage + enemyDamageModifier;
+
+            base.Start();
+        }
+
+        private void Update()
+        {
+            rb2d.velocity = new Vector2(velX, velY);
+        }
+
+        protected override void OnCantMove<T>(T component)
+        {
+            //throw new System.NotImplementedException();
+            if (component is Wall)
+            {
+                Wall hitWall = component as Wall;
+                hitWall.DamageWall(wallDamage);
+            }
+            else if (component is Enemy)
+            {
+                Enemy hitEnemy = component as Enemy;
+                hitEnemy.DamageEnemy(enemyDamage);
+            }
+        }
+
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            //all projectile colliding game objects should be tagged "Enemy" or whatever in inspector but that tag must be reflected in the below if conditional
+            if (col.gameObject.tag == "Wall")
+            {
+                Wall hitWall = col.GetComponent<Wall>();
+                hitWall.DamageWall(wallDamage);
+                Destroy(gameObject);
+            }
+            if (col.gameObject.tag == "OuterWall")
+            {
+                Destroy(gameObject);
+            }
+            if (col.gameObject.tag == "Enemy")
+            {
+                Enemy hitEnemy = col.GetComponent<Enemy>();
+                hitEnemy.DamageEnemy(enemyDamage);
+                Destroy(gameObject);
+            }
+        }
+
+        //protected override void AttemptMove<T>(int xDir, int yDir)
+        //{
+
+        //    base.AttemptMove<T>(xDir, yDir);
+
+        //    //Hit allows us to reference the result of the Linecast done in Move.
+        //    //RaycastHit2D hit;
+
+        //    //if (Move(xDir, yDir, out hit))
+        //    //{
+        //    //    SoundManager.instance.RandomizeSfx(moveSound1, moveSound2);
+        //    //}
+
+        //    //CheckIfGameOver();
+
+        //    //Set the playersTurn boolean of GameManager to false now that players turn is over.
+        //    //GameManager.instance.playersTurn = false;
+        //}
+
+    }
+}
